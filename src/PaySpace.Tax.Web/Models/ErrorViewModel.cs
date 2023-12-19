6777aca28702b680f24cf241af3019cefeb6a0e0
gerald.onnuigbo <gerald.onuigbo@fairmoney.io>
2023-12-19 17:03:42 +0100

    Add project files.

---
+++ src/PaySpace.Tax.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaySpace.Tax.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
